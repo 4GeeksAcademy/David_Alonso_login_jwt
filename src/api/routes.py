6ebca2e32222d33690e5862809a70739b52dbecc
David Alonso <125155276+DavidAlonsoRod@users.noreply.github.com>
2024-10-19 18:35:56 +0000

    login added

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,10 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +24,52 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/hello2', methods=['POST', 'GET'])
+def handle_hello2():
+
+    response_body = {
+        "message": "ESte es mi segundo hello"
+    }
+
+    return jsonify(response_body), 200
+
+# Create a route to authenticate your users and return JWTs. The
+# create_ s_token() function is used to actually generate the JWT.
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    user = User.query.filter_by (email=email).first()
+    print(user) 
+
+    if user == None:
+        return jsonify({"msg": "No encuentro tu email"}), 401
+    if email != user.email or password != user.password:
+        return jsonify({"msg": "Email o usuario incorrecto"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+@api.route("/signup", methods=["POST"])
+def signup():
+
+    body = request.get_json()
+    print(body)
+
+    user = User.query.filter_by(email=body["email"]).first()
+    print(user)
+    
+    if user == None:
+        user = User(email=body["email"], password=body["password"], is_active=False)
+        db.session.add(user)
+        db.session.commit()
+        response_body = {
+            "msg": "Usuario creado"
+        }
+        return jsonify(response_body), 200  # Indentación corregida
+    else:
+        return jsonify({"msg": "Ya tenemos fichado un cliente con ese corrreo"}), 401
+
+  
