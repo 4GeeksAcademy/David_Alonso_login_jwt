8e718a4c3ef843407e273c28ae6313eb5f84b704
David Alonso <125155276+DavidAlonsoRod@users.noreply.github.com>
2024-10-20 10:00:00 +0000

    Excercise finished

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -8,8 +8,8 @@ export const Navbar = () => {
 
 	const navigate = useNavigate()
 
-	const { store, actions } = useContext(Context); 
-	function handleLogout(){
+	const { store, actions } = useContext(Context);
+	function handleLogout() {
 		actions.logout()
 		navigate('/')
 
@@ -18,12 +18,10 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
+
 				<div className="ml-auto">
 					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+						<button className="btn btn-primary">Ir al contexto</button>
 					</Link>
 					<div>
 						{store.auth === true ? <button className="btn btn-primary" onClick={() => handleLogout()}>Logout</button> : ''}
